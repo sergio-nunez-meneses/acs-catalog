a5f0378321dfc06f3df5748ca461530e10ca631f
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-08-17 10:45:33 +0200

    first commit

---
+++ tools/tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE authors(
+  author_id INT AUTO_INCREMENT NOT NULL,
+  author_username VARCHAR (50) NOT NULL,
+  author_password VARCHAR (250) NOT NULL,
+  PRIMARY KEY (author_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE articles(
+  article_id INT AUTO_INCREMENT NOT NULL,
+  article_title VARCHAR (150) NOT NULL,
+  article_image VARCHAR (250) NOT NULL,
+  article_text TEXT NOT NULL,
+  DATETIME DATETIME NOT NULL,
+  article_archived BOOLEAN NOT NULL,
+  author_id INT NOT NULL,
+  PRIMARY KEY (article_id),
+  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
