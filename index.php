f0926885a06d2dd848f159c391bbe5cd745156d2
cyla00 <ikhayam000@protonmail.com>
2020-08-17 14:51:40 +0200

    article extr

--- index.php
+++ index.php
@@ -4,16 +4,38 @@ require("controllers/db.php");
 
 $db = new Database();
 $stmt = $db->run_query('SELECT * FROM articles');
-$data = $stmt->fetchAll();
+$articles = $stmt->fetchAll();
 
-echo $data[0]['article_id'] . "<br>";
-echo $data[0]['article_title'] . "<br>";
+foreach($articles as $article){
+  $pic = $article['article_image'];
+  echo "<div class='card'>";
+  echo "<form action='showArticle.php' method='post'>";
+  echo "<input type='submit' name='img' value='$pic'><br>";
 
-$pic = $data[0]['article_image'];
-echo "<img src='$pic' width='64' height='64'><br>";
+  echo $article['article_title'] . "<br>";
 
-echo $data[0]['article_text'] . "<br>";
-echo $data[0]['DATETIME'] . "<br>";
-echo $data[0]['article_archived'] . "<br>";
-echo $data[0]['article_id'] . "<br>";
- ?>
+  echo $article['DATETIME'] . "<br>";
+  echo "</form>";
+  echo "</div>";
+}
+
+
+
+function showArticle(){
+  $id = //##form input + format
+  $stmt = $db->run_query('SELECT * FROM articles WHERE article_id = :id', ['id' => $id]);
+  $article = $stmt->fetch();
+  echo "<div class='show_article'>";
+  echo "<h1 id='show_articleTitle'>" . $article['article_title'] . "</h1><br>";
+  echo "<h5 id='show_articleTime'>" . $article['DATETIME'] . "</h5>";
+
+  $show_img = $article['article_image'];
+  echo "<img src='$pic' width='400' height='400'><br>";
+
+  echo "<p>" . $article['article_text'] . "</p>";
+  echo "</div>";
+}
+
+
+
+?>
