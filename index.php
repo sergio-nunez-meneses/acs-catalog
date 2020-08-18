fe16a8388400c3af08995a397d1a215fdafe6be2
cyla00 <ikhayam000@protonmail.com>
2020-08-18 11:22:38 +0200

    ff

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
 <?php
 
-require("controllers/db.php");
-
-$db = new Database();
-$stmt = $db->run_query('SELECT * FROM articles');
-$articles = $stmt->fetchAll();
-
-foreach($articles as $article){
-  $pic = $article['article_image'];
-  echo "<div class='card'>";
-  echo "<a href='index.php'><img src='$pic'></a>";
-  echo $article['article_title'] . "<br>";
-
-  echo $article['DATETIME'] . "<br>";
-  echo "</div>";
-}
-
-
-
-function showArticle(){
-  $id = //##form input + format
-  $stmt = $db->run_query('SELECT * FROM articles WHERE article_id = :id', ['id' => $id]);
-  $article = $stmt->fetch();
-  echo "<div class='show_article'>";
-  echo "<h1 id='show_articleTitle'>" . $article['article_title'] . "</h1><br>";
-  echo "<h5 id='show_articleTime'>" . $article['DATETIME'] . "</h5>";
-
-  $show_img = $article['article_image'];
-  echo "<img src='$pic' width='400' height='400'><br>";
-
-  echo "<p>" . $article['article_text'] . "</p>";
-  echo "</div>";
-}
-
-
-
 ?>
