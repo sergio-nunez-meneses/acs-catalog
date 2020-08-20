8efedd729f76d58253e000fb00c484c5fafb3f34
cyla00 <ikhayam000@protonmail.com>
2020-08-20 11:08:26 +0200

    sql LIKE conversion

--- search.php
+++ search.php
@@ -7,36 +7,18 @@ $keyword = $_POST['search'];
 if(!empty($keyword)){
   $pdo = new Database;
   $keyword = $_POST['search'];
-  // $word = '';
 
-  // switch($word){
-  //   case is_array($keyword):
-  //   $word == $keyword[1];
-  //
-  //   case is_string($keyword):
-  //   $word == $$keyword;
-  // }
-
-  // $query_selection = "SELECT * FROM articles WHERE article_genre='in_array($word)' OR article_title='in_array($word)' OR article_text='in_array($word)' OR
-  // article_genre='$word' OR article_title='$word' OR article_text='$word'";
-
-$query_selection = "SELECT * FROM articles WHERE article_genre='$keyword' OR article_title='$keyword' OR article_text='$keyword'";
-
+  $query_selection = "SELECT * FROM articles WHERE article_genre LIKE '%$keyword%'";
   $articles = $pdo->run_query($query_selection)->fetchAll();
 
-  if(!empty($articles)){
-    foreach($articles as $article){
-      echo $article['article_image'] . "<br>";
-      echo $article['article_title'] . "<br>";
-      echo $article['DATETIME'] . "<br>";
-      echo $article['article_text'] . "<br>";
-      echo $article['author_id'] . "<br>";
-    }
+  foreach($articles as $article){
+    echo $article['article_image'] . "<br>";
+    echo $article['article_title'] . "<br>";
+    echo $article['DATETIME'] . "<br>";
+    echo $article['article_text'] . "<br>";
+    echo $article['author_id'] . "<br>";
   }
-  else{
-    header("Location: search.php");
-    echo "no posts found";
-  }
+
 }
 else{
   header("Location: index.php");
