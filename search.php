1efbbe0e1741da190121cd126e664a25ca1b812d
cyla00 <ikhayam000@protonmail.com>
2020-08-20 11:42:29 +0200

    search bar done

--- search.php
+++ search.php
@@ -8,17 +8,21 @@ if(!empty($keyword)){
   $pdo = new Database;
   $keyword = $_POST['search'];
 
-  $query_selection = "SELECT * FROM articles WHERE article_genre LIKE '%$keyword%'";
+  $query_selection = "SELECT * FROM articles WHERE article_title LIKE '%$keyword%' OR article_text LIKE '%$keyword%' OR article_genre LIKE '%$keyword%'";
   $articles = $pdo->run_query($query_selection)->fetchAll();
 
-  foreach($articles as $article){
-    echo $article['article_image'] . "<br>";
-    echo $article['article_title'] . "<br>";
-    echo $article['DATETIME'] . "<br>";
-    echo $article['article_text'] . "<br>";
-    echo $article['author_id'] . "<br>";
+  if(!empty($articles)){
+    foreach($articles as $article){
+      echo $article['article_image'] . "<br>";
+      echo $article['article_title'] . "<br>";
+      echo $article['DATETIME'] . "<br>";
+      echo $article['article_text'] . "<br>";
+      echo $article['author_id'] . "<br>";
+    }
   }
-
+  else{
+    echo "no posts have been found";
+  }
 }
 else{
   header("Location: index.php");
