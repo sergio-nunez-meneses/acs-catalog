c348854d2e386bc4458c6070c66e24fc8aedf058
cyla00 <ikhayam000@protonmail.com>
2020-08-25 09:40:46 +0200

    search.php image from database fix

--- actions/search.php
+++ actions/search.php
@@ -16,28 +16,30 @@ if (!empty($keyword)) {
   }
   if (!empty($articles)) {
     foreach($articles as $article) {
+      ?>
+      
+  <div class='search_pad'>
+    <div class='afterpad row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'>
 
-    echo "<div class='search_pad'>";
-      echo "<div class='afterpad row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'>";
+      <div class='img_wrapper img-fluid col-auto d-none d-lg-block'>
 
-      echo "<div class='img_wrapper img-fluid col-auto d-none d-lg-block'>";
+          <img src='<?php echo REL_PATH;?>public/img/<?php echo $article['article_image']; ?>'  class='search_img img-fluid bd-placeholder-img'  width='300' height='auto' xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid slice' focusable='false' role='img' aria-label='Placeholder: Thumbnail'>
 
-          echo "<img src='".$article['article_image']."' class='search_img img-fluid bd-placeholder-img'  width='300' height='auto' xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid slice' focusable='false' role='img' aria-label='Placeholder: Thumbnail'>";
+      </div>
 
-      echo "</div>";
+      <div class='col p-4 d-flex flex-column position-static'>
 
-        echo "<div class='col p-4 d-flex flex-column position-static'>";
+          <strong class='d-inline-block mb-2 text-primary'><?php echo $article['DATETIME'] ?></strong>
+          <h1 class='search_title'><?php echo $article['article_title']; ?></h1>
+          <h2 class='search_genre'><?php echo $article['article_genre']; ?></h2>
 
-          echo "<strong class='d-inline-block mb-2 text-primary'>" . $article['DATETIME'] . "</strong>";
-          echo "<h1 class='search_title'>" . $article['article_title'] . "</h1>";
-          echo "<h2 class='search_genre'>" . $article['article_genre'] . "</h2>";
+      </div>
 
-        echo "</div>";
+    </div>
 
-      echo "</div>";
+  </div>
 
-    echo "</div>";
-
+<?php
     }
   } else {
     echo "<div class='nopost_div'>";
